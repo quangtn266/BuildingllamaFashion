14fe3246b2b98ed78cd61336ebefec751e8388b7
quangtn266 <quangtrandn93@gmail.com>
2024-05-04 14:46:03 +0700

    Update source code for description

--- data/database_scrape.py
+++ data/database_scrape.py
@@ -3,48 +3,53 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-url_original = 'https://routine.vn/thoi-trang-nam/quan-nam/quan-jean-nam.html'
-
-product_list = []
-    #url_page = ((url) + str(page_number))
-
-    # Parse HTML and pull all href links
-    #response = requests.get(url_page)
-response = requests.get(url_original)
-main_page_items = BeautifulSoup(response.text, 'html.parser')
+url_original = 'https://routine.vn/thoi-trang-nam.html'  #'https://dirtycoins.vn/t-shirts-polos'
 
 hrefs = []
 data_product_skus = []
 prices = []
 items = []
 brands = []
+descriptions = []
 
-products = main_page_items.find_all('div', class_ ='product details product-item-details')
+for num in range(1, 100):
+    if num == 1:
+        url = url_original
+    else:
+        url = url_original + "?p=%d".format(num)
 
-#print(main_page_items)
+    response = requests.get(url_original)
+    main_page_items = BeautifulSoup(response.text, 'html.parser')
+    products = main_page_items.find_all('div', class_ ='product details product-item-details')
 
-for i in products:
-    link = i.find("a", class_="product-item-link").get("href")
-    brand = link.split("https://")[1].split("/")[0]
-    price = i.find("span", class_="price-wrapper").get("data-price-amount")
-    data_product_sku = i.find("div").get("data-product-id")
+    for i in products:
+        link = i.find("a", class_="product-item-link").get("href")
+        brand = link.split("https://")[1].split("/")[0]
+        price = i.find("span", class_="price-wrapper").get("data-price-amount")
+        data_product_sku = i.find("div").get("data-product-id")
 
-    hrefs.append(link)
-    prices.append(price)
-    data_product_skus.append(data_product_sku)
-    brands.append(brand)
+        hrefs.append(link)
+        prices.append(price)
+        data_product_skus.append(data_product_sku)
+        brands.append(brand)
 
-    onclicks = i.find("a").get("onclick")
-    item_list_names = onclicks[onclicks.index("item_list_name") + len("item_list_name"): onclicks.index("items")]
-    items.append(item_list_names)
+        onclicks = i.find("a").get("onclick")
+        item_list_names = onclicks[onclicks.index("item_list_name") + len("item_list_name"): onclicks.index("items")]
+        item_list_names = item_list_names.removeprefix('":"').replace('","', '')
+        items.append(item_list_names)
+
+        description = i.find("a")
+        descriptions.append(description.text)
+
+        #print(i.find("a"))
+
 
 basic_scrape = pd.DataFrame({
             'Link': hrefs,
             'Category_Type': items,
-            'Product_sku': data_product_skus,
+            'Product_ID': data_product_skus,
+            'Description': descriptions,
             'Price': prices,
             'Brand': brands})
-
-#print(basic_scrape)
 
 basic_scrape.to_csv('./page_number.csv', index=False, header=True)
